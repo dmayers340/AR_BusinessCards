c7afe4cc3892a1f091a3884d104877a672cf88f5
Deb <dmayers340@gmail.com>
2018-10-25 19:35:21 +0100

    Adding initial project

---
+++ Assets/Scripts/BouncingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingObject : MonoBehaviour {
+
+    public float speed;
+    public float height;
+
+ 
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        float newYValue = Mathf.Sin(Time.time * speed);
+
+        transform.position = new Vector3(position.x, newYValue, position.z) * height; 
+    }
+
+}
