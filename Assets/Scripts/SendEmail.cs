b7c7896b4a02b0382683ebc4c8b0976aed7d1e21
Deb <dmayers340@gmail.com>
2018-10-30 09:19:58 +0000

    Working on button presses

---
+++ Assets/Scripts/SendEmail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendEmail : MonoBehaviour {
+
+    public void EmailToSend()
+    {
+        string email = "dmayers340@gmail.com";
+        string subject = MyEscapeURL("Email After Recieving AR Business Car");
+        string body = MyEscapeURL("Here is a quick email to send\r\nIt was great to see your business cards");
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+    }
+    string MyEscapeURL(string url)
+    {
+        return WWW.EscapeURL(url).Replace("+", "%20");
+    }
+}
